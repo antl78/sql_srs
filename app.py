7bfbf60ebe10bed4dd64fa41ee62d725cca0f11c
Lay78 <antoine_lay@yahoo.fr>
2026-03-19 16:59:36 +0100

    add sidebar

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ st.write("""
 Spaced Repetition System SQL Practice
 """)
 
-option = st.selectbox(
-    "What would you like to review?",
-    ("Joins", "GroupBy", "Windows Functions"),
-    index=None,
-    placeholder="Select a theme...",
-)
-
-st.write('You selected:', option)
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to review?",
+        ("Joins", "GroupBy", "Windows Functions"),
+        index=None,
+        placeholder="Select a theme...",
+    )
+    st.write('You selected:', option)
 
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
