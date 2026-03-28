9a98bd95bd949349640228f118deab38a3c4aec1
Lay78 <antoine_lay@yahoo.fr>
2026-03-28 11:16:58 +0100

    add first question

--- app.py
+++ app.py
@@ -1,49 +1,48 @@
 import pandas as pd
 import streamlit as st
 import duckdb
+import io
 
-st.write("""
-# SQL SRS
-Spaced Repetition System SQL Practice
-""")
+csv = '''
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+'''
 
-option = st.selectbox(
-    "What would you like to review?",
-    ("Joins", "GroupBy", "Windows Functions"),
-    index=None,
-    placeholder="Select a theme...",
-)
+beverages = pd.read_csv(io.StringIO(csv))
 
-st.write('You selected:', option)
+csv2 = '''
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+'''
 
-data = {"a": [1, 2, 3], "b": [4, 5, 6]}
-df = pd.DataFrame(data)
+food_items = pd.read_csv(io.StringIO(csv2))
 
-# Connexion DuckDB en mémoire et enregistrement du DataFrame
-con = duckdb.connect()
-con.register("df", df)
+answer = """
+SELECT * FROM beverages
+CROSS JOIN food_items
+"""
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+solution = duckdb.sql(answer).df()
 
-with tab1:
-    st.subheader("Requête SQL sur le DataFrame")
-    st.caption("Table disponible : `df` — colonnes : `a`, `b`")
+st.header("Enter your code:")
+query = st.text_area(label="Votre code SQL ici", key="user_input")
+if query:
+    result = duckdb.sql(query).df()
+    st.dataframe(result)
 
-    sql_query = st.text_input("Entrez votre requête SQL", value="SELECT * FROM df")
-    st.write(f"Vous avez entré la query suivante : {sql_query}")
-
-    if sql_query:
-        try:
-            result = con.execute(sql_query).df()
-            st.success(f"{len(result)} ligne(s) retournée(s)")
-            st.dataframe(result)
-        except Exception as e:
-            st.error(f"Erreur SQL : {e}")
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    st.write("table: beverages")
+    st.dataframe(beverages)
+    st.write("table: food_items")
+    st.dataframe(food_items)
+    st.write("expected:")
+    st.dataframe(solution)
 
 with tab3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+    st.write(answer)
