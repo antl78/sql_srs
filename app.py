a5d512f70681123e903f566c9a0a3d729084c80e
Lay78 <antoine_lay@yahoo.fr>
2026-04-15 10:56:16 +0200

    use user input to display exercises specifics

--- app.py
+++ app.py
@@ -4,13 +4,8 @@ import io
 import duckdb
 import pandas as pd
 import streamlit as st
+import ast
 
-
-
-ANSWER_STR = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
@@ -20,7 +15,7 @@ con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=Fals
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to review?",
-        ("cross_joins", "GroupBy", "Window Functions"),
+        ("cross_joins", "GroupBy", "window_functions"),
         index=None,
         placeholder="Select a theme...",
     )
@@ -31,6 +26,16 @@ with st.sidebar:
 
 st.header("Enter your code:")
 query = st.text_area(label="Votre code SQL ici", key="user_input")
+
+if st.button("Valider"):
+    if query:
+        try:
+            result = con.execute(query).df()
+            st.dataframe(result)
+        except Exception as e:
+            st.error(f"Erreur SQL : {e}")
+    else:
+        st.warning("Veuillez entrer une requête SQL.")
 
 # if query:
 #     result = duckdb.sql(query).df()
@@ -54,15 +59,29 @@ query = st.text_area(label="Votre code SQL ici", key="user_input")
 #         )
 #
 #
-# tab2, tab3 = st.tabs(["Tables", "Solution"])
-#
-# with tab2:
-#     st.write("table: beverages")
-#     st.dataframe(beverages)
+
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+with tab2:
+    if theme and not exercise.empty:
+        exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+        for table in exercise_tables:
+            st.write(f"table: {table}")
+            df_table = con.execute(f"SELECT * FROM {table}").df()
+            st.dataframe(df_table)
+    else:
+        st.write("Please select a theme to see the tables.")
+
+with tab3:
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+        print(answer)
+    st.write(answer)
+
 #     st.write("table: food_items")
 #     st.dataframe(food_items)
 #     st.write("expected:")
 #     st.dataframe(solution_df)
 #
-# with tab3:
-#     st.write(ANSWER_STR)
+
