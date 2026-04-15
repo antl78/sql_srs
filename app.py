c793b7720abe281b819dfe1a02e775ae4b50590e
Lay78 <antoine_lay@yahoo.fr>
2026-04-15 17:12:18 +0200

    comparison with real answer

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
 
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
+
 st.header("Enter your code:")
 query = st.text_area(label="Votre code SQL ici", key="user_input")
 
@@ -36,29 +42,6 @@ if st.button("Valider"):
             st.error(f"Erreur SQL : {e}")
     else:
         st.warning("Veuillez entrer une requête SQL.")
-
-# if query:
-#     result = duckdb.sql(query).df()
-#     st.dataframe(result)
-#
-#     if len(result.columns) != len(
-#         solution_df.columns
-#     ):  # replace with try result = result[solution_df.columns]
-#         st.write("Some columns are missing")
-#
-#     try:
-#         result = result[solution_df.columns]
-#         st.dataframe(result.compare(solution_df))
-#     except KeyError as e:
-#         st.write("Some columns are missing")
-#
-#     n_lines_difference = result.shape[0] - solution_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(
-#             f"result has a difference of {n_lines_difference} lines with the solution"
-#         )
-#
-#
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
@@ -73,15 +56,4 @@ with tab2:
         st.write("Please select a theme to see the tables.")
 
 with tab3:
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{exercise_name}.sql", "r") as f:
-        answer = f.read()
-        print(answer)
     st.write(answer)
-
-#     st.write("table: food_items")
-#     st.dataframe(food_items)
-#     st.write("expected:")
-#     st.dataframe(solution_df)
-#
-
